6f1d8a8e38a1f25477b32e74a959b91cf3b69546
Nazarii Voitkiv <vojtkivnazar2005@gmail.com>
2025-10-30 07:09:54 +0100

    add burn/pause/ownable

--- contracts/MyTokenERC20.sol
+++ contracts/MyTokenERC20.sol
@@ -2,11 +2,23 @@
 pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract MyToken is ERC20 {
+contract MyToken is ERC20, ERC20Burnable, Ownable, Pausable {
     constructor(string memory name_, string memory symbol_, uint256 initialSupplyWhole)
         ERC20(name_, symbol_)
+        Ownable(msg.sender)
     {
         _mint(msg.sender, initialSupplyWhole * 10 ** decimals());
+    }
+
+    function pause() external onlyOwner { _pause(); }
+    function unpause() external onlyOwner { _unpause(); }
+
+    function _update(address from, address to, uint256 value) internal override {
+        require(!paused(), "Token is paused");
+        super._update(from, to, value);
     }
 }
